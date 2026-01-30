69946f04aef1e312a8ca6277f75dadd68b3672ab
Zach Kushnir <zskushnir@gmail.com>
2026-01-30 13:12:01 -0500

    new

--- Scripts/stretch_test.py
+++ Scripts/stretch_test.py
@@ -24,31 +24,31 @@ try:
 
     robot.end_of_arm.move_to('wrist_yaw', np.radians(30))
     robot.push_command()
-    time.sleep(1.0)
+    time.sleep(5.0)
 
     robot.end_of_arm.move_to('wrist_pitch', np.radians(30))
     robot.push_command()
-    time.sleep(1.0)
+    time.sleep(5.0)
 
     robot.end_of_arm.move_to('wrist_roll', np.radians(30))
     robot.push_command()
-    time.sleep(1.0)
+    time.sleep(5.0)
 
     robot.end_of_arm.move_to('stretch_gripper', 50)
     robot.push_command()
-    time.sleep(1.0)
+    time.sleep(5.0)
 
     robot.end_of_arm.move_to('stretch_gripper', 0)
     robot.push_command()
-    time.sleep(1.0)
+    time.sleep(5.0)
 
     robot.head.move_by('head_pan', np.radians(45))
     robot.push_command()
-    time.sleep(1.0)
+    time.sleep(5.0)
 
     robot.head.move_by('head_tilt', np.radians(45))
     robot.push_command()
-    time.sleep(1.0)
+    time.sleep(5.0)
 
     robot.stow()
     robot.push_command()
@@ -58,15 +58,15 @@ try:
     # Base moves: use sleeps (or check base status), and they won't run if bump is active
     robot.base.translate_by(0.5)
     robot.push_command()
-    time.sleep(3.0)
+    time.sleep(5.0)
 
     robot.base.rotate_by(np.radians(180))
     robot.push_command()
-    time.sleep(4.0)
+    time.sleep(10.0)
 
     robot.base.translate_by(0.5)
     robot.push_command()
-    time.sleep(3.0)
+    time.sleep(5.0)
 
 finally:
     robot.stop()
