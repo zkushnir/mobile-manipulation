0bde009a916ae31105ebb44cc114d41e6213803d
Zach Kushnir <zskushnir@gmail.com>
2026-01-30 13:03:23 -0500

    new

--- Scripts/stretch_test.py
+++ Scripts/stretch_test.py
@@ -1,16 +1,72 @@
 import time
 import numpy as np
 import stretch_body.robot
+
 robot = stretch_body.robot.Robot()
-robot.startup()
 
-robot.stow()
-robot.push_command()
-robot.arm.wait_until_at_setpoint()
+try:
+    robot.startup()
 
-# Move to full extension and height
-robot.arm.move_to(0.5)
-robot.lift.move_to(0.5)
-robot.push_command()
-robot.arm.wait_until_at_setpoint() # Wait for motion to complete
+    if not robot.is_calibrated():
+        print("Robot not calibrated. Run: stretch_robot_home.py (and wrist/gripper home if needed)")
+        raise SystemExit(1)
 
+    robot.stow()
+    robot.push_command()
+    robot.arm.wait_until_at_setpoint()
+    robot.lift.wait_until_at_setpoint()
+
+    robot.arm.move_to(0.5)
+    robot.lift.move_to(0.5)
+    robot.push_command()
+    robot.arm.wait_until_at_setpoint()
+    robot.lift.wait_until_at_setpoint()
+
+    robot.end_of_arm.move_to('wrist_yaw', np.radians(30))
+    robot.push_command()
+    time.sleep(1.0)
+
+    robot.end_of_arm.move_to('wrist_pitch', np.radians(30))
+    robot.push_command()
+    time.sleep(1.0)
+
+    robot.end_of_arm.move_to('wrist_roll', np.radians(30))
+    robot.push_command()
+    time.sleep(1.0)
+
+    robot.end_of_arm.move_to('stretch_gripper', 50)
+    robot.push_command()
+    time.sleep(1.0)
+
+    robot.end_of_arm.move_to('stretch_gripper', 0)
+    robot.push_command()
+    time.sleep(1.0)
+
+    robot.head.move_by('head_pan', np.radians(45))
+    robot.push_command()
+    time.sleep(1.0)
+
+    robot.head.move_by('head_tilt', np.radians(45))
+    robot.push_command()
+    time.sleep(1.0)
+
+    robot.stow()
+    robot.push_command()
+    robot.arm.wait_until_at_setpoint()
+    robot.lift.wait_until_at_setpoint()
+
+    # Base moves: use sleeps (or check base status), and they won't run if bump is active
+    robot.base.translate_by(0.5)
+    robot.push_command()
+    time.sleep(3.0)
+
+    robot.base.rotate_by(np.radians(180))
+    robot.push_command()
+    time.sleep(4.0)
+
+    robot.base.translate_by(0.5)
+    robot.push_command()
+    time.sleep(3.0)
+
+finally:
+    robot.stop()
